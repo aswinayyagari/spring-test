bc120075928c3fdd90764c1f0526618d40f95f1a
aayyagari <aayyagari@innominds.com>
2017-06-02 19:24:46 +0530

    initial skeleton

---
+++ src/main/java/com/aswini/test/springjavaconfig/SpringJavaConfigApplication.java
@@ -0,0 +1,12 @@
+package com.aswini.test.springjavaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaConfigApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aswini/test/springjavaconfig/SpringJavaConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aswini.test.springjavaconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJavaConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
